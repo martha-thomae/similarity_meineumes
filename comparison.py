b0a0d6066ebe58874891ccc34a73264e686cbdaa
martha-thomae <thomaemartha@gmail.com>
2023-12-20 17:19:34 +0100

    Add support for csv, and a few formatting stuff: display the levenshtein distance result, and also edit some of the comments of to-dos

--- comparison.py
+++ comparison.py
@@ -32,25 +32,49 @@ if __name__ == "__main__":
 
     # CSV file containing the list of the files to cmpare
     if(args.csvfile):
-        pass
+        all_entries = []
+        with open(args.csvfile, encoding='utf-8-sig') as csvfile:
+            csv_content = csv.reader(csvfile, delimiter=';', quotechar='\n')
+            for row in csv_content:
+                all_entries.append(row)
+        csvfile.close()
+        print(all_entries)
+
+        for row in all_entries:
+            meidoc_A = minidom.parse("../GABCtoMEI/MEI_outfiles/MEI_intermedfiles/" + row[0] + ".mei")
+            contour_A = getContour(meidoc_A)
+            meidoc_B = minidom.parse("../GABCtoMEI/MEI_outfiles/MEI_intermedfiles/" + row[1] + ".mei")
+            contour_B = getContour(meidoc_B)
+            #### TO DO: Eventually substitute by "MEI_outfiles," this would mean to translate pitches to contour for square notation files
+
+            # Calculate Levenshtein distance
+            distance = Levenshtein.distance(contour_A, contour_B)
+            # Print the results
+            print(row[0], row[1], distance)
+            for i in range(0, max(len(contour_A),len(contour_B))):
+                if (i < len(contour_A) and i < len(contour_B)):
+                    print(contour_A[i], contour_B[i], Levenshtein.distance(contour_A[:i+1], contour_B[:i+1]))
+                elif (i >= len(contour_A) and i < len(contour_B)):
+                    print(None, contour_B[i], Levenshtein.distance(contour_A[:i+1], contour_B[:i+1]))
+                elif (i < len(contour_A) and i >= len(contour_B)):
+                    print(contour_A[i], None, Levenshtein.distance(contour_A[:i+1], contour_B[:i+1]))
+                # else: doesn't happen
+            print("\n")
 
     # Two files to compare
     else:
         # First file
-        #### TO DO: Eventually substitute by "MEI_outfiles," this would mean to translate pitches to contour for square notation files
         meidoc_A = minidom.parse("../GABCtoMEI/MEI_outfiles/MEI_intermedfiles/" + args.file1 + ".mei")
-        contour_A = getContour(meidoc_A)
-        print(contour_A)
-
+        contour_A = getContour(meidoc_A) #print(contour_A)
         # Second file
-        #### TO DO: Eventually substitute by "MEI_outfiles," this would mean to translate pitches to contour for square notation files
         meidoc_B = minidom.parse("../GABCtoMEI/MEI_outfiles/MEI_intermedfiles/" + args.file2 + ".mei")
-        contour_B = getContour(meidoc_B)
-        print(contour_B)
+        contour_B = getContour(meidoc_B) #print(contour_B)
+        #### TO DO: Eventually substitute by "MEI_outfiles," this would mean to translate pitches to contour for square notation files
 
         # Calculate Levenshtein distance
-        Levenshtein.distance(contour_A, contour_B)
+        distance = Levenshtein.distance(contour_A, contour_B)
         # Print the results
+        print(args.file1, args.file2, distance)
         for i in range(0, max(len(contour_A),len(contour_B))):
             if (i < len(contour_A) and i < len(contour_B)):
                 print(contour_A[i], contour_B[i], Levenshtein.distance(contour_A[:i+1], contour_B[:i+1]))
