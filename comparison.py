897441d95d8907125b8dc2d95dc4ae55e9bbef54
martha-thomae <thomaemartha@gmail.com>
2023-12-26 19:45:17 +0100

    Change from edit distance to Needleman- Wunsch

--- comparison.py
+++ comparison.py
@@ -2,7 +2,7 @@ import argparse
 import csv
 from xml.dom import minidom
 from difflib import SequenceMatcher
-import Levenshtein
+from minineedle import needle, smith, core
 
 # --------- #
 # Functions #
@@ -40,7 +40,7 @@ if __name__ == "__main__":
     parser.add_argument('-csvfile', help='csv file listing all the MEI Neumes files to compare', default=None)
     args = parser.parse_args()
 
-    # CSV file containing the list of the files to cmpare
+    # CSV file containing the list of the files to compare
     if(args.csvfile):
         all_entries = []
         with open(args.csvfile, encoding='utf-8-sig') as csvfile:
@@ -50,8 +50,6 @@ if __name__ == "__main__":
         csvfile.close()
         print(all_entries)
 
-
-        all_results = []
         for row in all_entries:
             meidoc_A = minidom.parse("../GABCtoMEI/MEI_outfiles/MEI_intermedfiles/" + row[0] + ".mei")
             contour_A, syltexts_A = getContour(meidoc_A)
@@ -59,48 +57,28 @@ if __name__ == "__main__":
             contour_B, syltexts_B = getContour(meidoc_B)
             #### TO DO: Eventually substitute by "MEI_outfiles," this would mean to translate pitches to contour for square notation files
 
-            # Calculate Levenshtein distance
-            distance = Levenshtein.distance(contour_A, contour_B)
+            # Calculate alignment
+            alignment = needle.NeedlemanWunsch(contour_A, contour_B)
+            alignment.gap_character = "x"
             # Print the results
-            all_results.append(['\n'+row[0], '', row[1], '', "DISTANCE = " + str(distance)])
-            for i in range(0, max(len(contour_A),len(contour_B))):
-                if (i < len(contour_A) and i < len(contour_B)):
-                    results = [syltexts_A[i], contour_A[i], syltexts_B[i], contour_B[i], Levenshtein.distance(contour_A[:i+1], contour_B[:i+1])]
-                elif (i >= len(contour_A) and i < len(contour_B)):
-                    results = [None, None, syltexts_B[i], contour_B[i], Levenshtein.distance(contour_A[:i+1], contour_B[:i+1])]
-                elif (i < len(contour_A) and i >= len(contour_B)):
-                    results = [syltexts_A[i], contour_A[i], None, None, Levenshtein.distance(contour_A[:i+1], contour_B[:i+1])]
-                # else:
-                    # doesn't happen
-                all_results.append(results)
-            all_results.append(['\n', '\n', '\n', '\n', '\n'])
-        print(all_results)
+            print(row[0], row[1], alignment.get_score())
+            print(alignment)
+            print()
 
-        with open("crosscomparison_results.csv", "w") as f:
-            writer = csv.writer(f)
-            writer.writerows(all_results)
-        f.close()
 
     # Two files to compare
     else:
         # First file
         meidoc_A = minidom.parse("../GABCtoMEI/MEI_outfiles/MEI_intermedfiles/" + args.file1 + ".mei")
-        contour_A = getContour(meidoc_A) #print(contour_A)
+        contour_A, syltexts_A = getContour(meidoc_A) #print(contour_A)
         # Second file
         meidoc_B = minidom.parse("../GABCtoMEI/MEI_outfiles/MEI_intermedfiles/" + args.file2 + ".mei")
-        contour_B = getContour(meidoc_B) #print(contour_B)
+        contour_B, syltexts_B = getContour(meidoc_B) #print(contour_B)
         #### TO DO: Eventually substitute by "MEI_outfiles," this would mean to translate pitches to contour for square notation files
 
-        # Calculate Levenshtein distance
-        distance = Levenshtein.distance(contour_A, contour_B)
+        # Calculate alignment
+        alignment = needle.NeedlemanWunsch(contour_A, contour_B)
+        alignment.gap_character = "x"
         # Print the results
-        print(args.file1, args.file2, distance)
-        for i in range(0, max(len(contour_A),len(contour_B))):
-            if (i < len(contour_A) and i < len(contour_B)):
-                print(contour_A[i], contour_B[i], Levenshtein.distance(contour_A[:i+1], contour_B[:i+1]))
-            elif (i >= len(contour_A) and i < len(contour_B)):
-                print(None, contour_B[i], Levenshtein.distance(contour_A[:i+1], contour_B[:i+1]))
-            elif (i < len(contour_A) and i >= len(contour_B)):
-                print(contour_A[i], None, Levenshtein.distance(contour_A[:i+1], contour_B[:i+1]))
-            # else:
-                # doesn't happen
+        print(args.file1, args.file2, alignment.get_score())
+        print(alignment)
